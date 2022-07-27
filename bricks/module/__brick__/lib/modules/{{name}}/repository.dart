099f3fdff67ebe74e5c9d7fa5aed6769e28f4f38
quandm <daominhquan176@gmail.com>
2022-07-27 10:48:15 +0700

    update

--- bricks/module/__brick__/lib/modules/{{name}}/repository.dart
+++ bricks/module/__brick__/lib/modules/{{name}}/repository.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
+import '../../models/base_model.dart';
 import '../../models/user_model.dart';
+import '../../repositories/base_repository.dart';
 import '../../services/api_service.dart';
 
 abstract class I{{name.pascalCase()}}Repository  with MBMixinRepository<UserModel> {
@@ -7,6 +9,14 @@ abstract class I{{name.pascalCase()}}Repository  with MBMixinRepository<UserMode
 }
 
 class {{name.pascalCase()}}Repository extends I{{name.pascalCase()}}Repository{
+  IApiService get api => Get.find<IApiService>();
 
-final IApiService get api => Get.find<IApiService>();
+  @override
+  // TODO: implement decoder
+  BaseModel Function(Map<String, dynamic> p1) get decoder =>
+      throw UnimplementedError();
+
+  @override
+  // TODO: implement path
+  String get path => throw UnimplementedError();
 } 
